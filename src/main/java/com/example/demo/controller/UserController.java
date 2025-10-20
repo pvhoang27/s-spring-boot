1a78a3e39b115f6a26a5348462dc7c22c704966a
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-10-21 00:17:08 +0700

    update

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+import com.example.demo.dto.request.UserUpdateRequest;
+import com.example.demo.entity.User;
+
+
+import com.example.demo.dto.request.UserCreationRequest;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request){
+         return  userService.createUser(request);
+
+    }
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request   );
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId){
+        userService.deleteUsers(userId);
+        return "User has been deleted";
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.dto.request.UserCreationRequest;
+import com.example.demo.dto.request.UserUpdateRequest;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private  UserRepository userRepository ;
+
+    public User createUser(UserCreationRequest request){
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+
+    }
+    public  User updateUser(String userId, UserUpdateRequest request){
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return  userRepository.save(user);
+    }
+    public void deleteUsers(String userId){
+        userRepository.deleteById(userId);
+    }
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+    public  User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found "));
+    }
+}
